227c7758a32c47940a0fa661d3650c45a690030a
ssykz5 <100894600+ssykz5@users.noreply.github.com>
2023-01-14 21:22:11 +0800

    Version 0.0 completed

--- coursework_part4_data_processing_and_visualization/GUI_main.py
+++ coursework_part4_data_processing_and_visualization/GUI_main.py
@@ -21,26 +21,26 @@ class DataPlotting(tk.Tk):
         # self.analysis = Analysis()
 
         # Set the title of the main window
-        self.title("Plotting")
+        self.title("Indoor Temperature Plotting")
 
         # Set the size of the main window.
-        self.width = 1200
-        self.height = 720
+        self.width = 1400
+        self.height = 550
         size_geo = '%dx%d+%d+%d' % (self.width, self.height, (self.winfo_screenwidth()-self.width)/2, (self.winfo_screenheight()-self.height)/2)
         self.geometry(size_geo)
 
         # Change the background colour.
-        self.config(background="#6fb765")
+        self.config(background="#F57C00")
         # Set the window at the top level.
         # self.attributes('-topmost',True)
         # Set the transparency of the window.
         self.attributes('-alpha',1)
-        # Change the icon in the top left corner of the window
+        # Change the icon in the top left corner of the window 
         self.iconbitmap('.\\Tower.ico')
 
         # Set the software title.
         self.title_size = self.winfo_width() // 20
-        software_title=tk.Label(self, text="Indoor Temperature Plotting", bg="yellow", fg="red", font=('Times', f"{self.title_size}", 'bold italic underline'))
+        software_title=tk.Label(self, text="Indoor Temperature Plotting", bg="#FF9800", fg="#FFE0B2", font=('Times', f"{self.title_size}", 'bold italic'))
         software_title.pack(side=tk.TOP)
 
         # Initialize the original directories.
@@ -63,25 +63,25 @@ class DataPlotting(tk.Tk):
         self.end_time.set('18:00')
 
         # Create a PanedWindow for storing other PanedWindow.
-        self.p_control = tk.PanedWindow(self, orient=tk.VERTICAL)
+        self.p_control = tk.PanedWindow(self, orient=tk.VERTICAL, bg='#FF9800')
         self.p_control.pack(fill=tk.NONE, expand=1, side=tk.TOP, anchor="n")
 
         # Create a PanedWindow for creating the analysis.
-        self.p_create = tk.PanedWindow(self)
+        self.p_create = tk.PanedWindow(self, bg='#FF9800')
         # self.p_create.pack(fill=tk.NONE, expand=1, side=tk.TOP, anchor="n")
         self.p_control.add(self.p_create)
 
         # -----------------
         # For choosing target directory.
         # ----------------
-        tfd_label = tk.Label(self.p_create, text="Target File Directory:", bg="#6fb765", fg="white")
-        tfd_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        tfd_label = tk.Label(self.p_create, text="Target File Directory:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        tfd_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Directory Entry
-        directory_entry = tk.Entry(self.p_create, textvariable=self.directory, state="readonly", bg="green")
-        directory_entry.pack(fill=tk.X, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        directory_entry = tk.Entry(self.p_create, textvariable=self.directory, state="readonly", bg="#FF9800")
+        directory_entry.pack(fill=tk.X, side=tk.LEFT, padx=20, pady=20)
         # Choose button
-        choose_dir_btn = tk.Button(self.p_create, text="Choose Directory", command=self.select_directory_input, bg="green", fg="white")
-        choose_dir_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        choose_dir_btn = tk.Button(self.p_create, text="Choose Directory", command=self.select_directory_input, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
+        choose_dir_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # --------------
         # For analysis name decision.
@@ -98,26 +98,26 @@ class DataPlotting(tk.Tk):
         # -------------
         # For choosing output directory
         # -------------
-        output_label = tk.Label(self.p_create, text="Output File Directory:", bg="#6fb765", fg="white")
-        output_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        output_label = tk.Label(self.p_create, text="Output File Directory:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        output_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Output directory Entry
-        output_entry = tk.Entry(self.p_create, textvariable=self.output_dir, state="readonly", bg="green")
-        output_entry.pack(fill=tk.X, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        output_entry = tk.Entry(self.p_create, textvariable=self.output_dir, state="readonly", bg="#FF9800")
+        output_entry.pack(fill=tk.X, side=tk.LEFT, padx=20, pady=20)
         # Choose button
-        output_choose_dir_btn = tk.Button(self.p_create, text="Choose Directory", command=self.select_directory_output, bg="green", fg="white")
-        output_choose_dir_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        output_choose_dir_btn = tk.Button(self.p_create, text="Choose Directory", command=self.select_directory_output, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
+        output_choose_dir_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # Initialize the analysis name.
         self.analysis_name = tk.StringVar()
         self.analysis_name.set("New Analysis")
         # Create analysis button
-        create_analysis_btn = tk.Button(self.p_create, text="Create Analysis", command=self.initialize_analysis, bg="green", fg="white")
-        create_analysis_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        create_analysis_btn = tk.Button(self.p_create, text="Create Analysis", command=self.initialize_analysis, bg="#FF9800", fg="#212121", font=('Times', 10, 'bold'))
+        create_analysis_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # -------------
         # Input the outdoor temperature.
         # ------------
-        self.p_outdoor = tk.PanedWindow(self)
+        self.p_outdoor = tk.PanedWindow(self, bg='#FF9800')
         self.p_control.add(self.p_outdoor)
 
         # Create a variable for storing the outdoor temperature csv directory.
@@ -125,82 +125,84 @@ class DataPlotting(tk.Tk):
         # Set the default value.
         self.outdoor_temp_dir.set("Please Choose the Outdoor Temperature File.")
         # Show the outdoor_temperature directory.
-        outdoor_temp_entry = tk.Entry(self.p_outdoor, textvariable=self.outdoor_temp_dir, state="readonly", bg="green", width=40)
-        outdoor_temp_entry.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        outdoor_label = tk.Label(self.p_outdoor, text="Outdoor Temperature Directory:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        outdoor_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
+        outdoor_temp_entry = tk.Entry(self.p_outdoor, textvariable=self.outdoor_temp_dir, state="readonly", bg="#FF9800", width=40)
+        outdoor_temp_entry.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Button used to choose outdoor temperature file.
-        outdoor_temp_btn = tk.Button(self.p_outdoor, text="Choose Outdoor Temperature File", command=self.select_outdoor_temp_input)
-        outdoor_temp_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        outdoor_temp_btn = tk.Button(self.p_outdoor, text="Choose Directory", command=self.select_outdoor_temp_input, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
+        outdoor_temp_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Add outdoor temperature df into the analysis.
-        add_outdoor_temp_btn = tk.Button(self.p_outdoor, text="Add Outdoor Temperature datas into the Analysis", command=self.add_outdoor_temperature_df)
-        add_outdoor_temp_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        add_outdoor_temp_btn = tk.Button(self.p_outdoor, text="Add Outdoor Temperature datas into the Analysis", command=self.add_outdoor_temperature_df, bg="#FF9800", fg="#FFE0B2", font=('Times', 10, 'bold'))
+        add_outdoor_temp_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # ------------
         # Plot Graphs.
         # ------------
         # Create a PanedWindow for choosing the start date and time.
-        self.p_dt = tk.PanedWindow(self)
+        self.p_dt = tk.PanedWindow(self, bg='#FF9800')
         self.p_control.add(self.p_dt)
 
-        self.p_dt_start = tk.PanedWindow(self)
+        self.p_dt_start = tk.PanedWindow(self, bg='#FF9800')
         # self.p_dt_start.pack(fill=tk.NONE, expand=1, anchor="n", side=tk.TOP)
         self.p_dt.add(self.p_dt_start)
         # Choose start and end dates.
         # Showing start date.
-        start_date_indicator = tk.Label(self.p_dt_start, text="Start Date:")
-        start_date_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
-        start_date_label = tk.Label(self.p_dt_start, textvariable=self.start_date)
-        start_date_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        start_date_indicator = tk.Label(self.p_dt_start, text="Start Date:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        start_date_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
+        start_date_label = tk.Label(self.p_dt_start, textvariable=self.start_date, bg="#FF9800", fg="#FFF9C4", font=('Times', 13, 'bold'))
+        start_date_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Showing start time.
-        start_time_indicator = tk.Label(self.p_dt_start, text="Start time:")
-        start_time_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
-        start_time_label = tk.Label(self.p_dt_start, textvariable=self.start_time)
-        start_time_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        start_time_indicator = tk.Label(self.p_dt_start, text="Start time:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        start_time_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
+        start_time_label = tk.Label(self.p_dt_start, textvariable=self.start_time, bg="#FF9800", fg="#FFF9C4", font=('Times', 13, 'bold'))
+        start_time_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Choose start date and time.
-        start_date_btn = tk.Button(self.p_dt_start, text="Choose start date & time", command=self.choose_start_date_and_time)
-        start_date_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        start_date_btn = tk.Button(self.p_dt_start, text="Choose start date & time", command=self.choose_start_date_and_time, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
+        start_date_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # Create a PanedWindow for choosing the end date and time.
-        self.p_dt_end = tk.PanedWindow(self)
+        self.p_dt_end = tk.PanedWindow(self, bg='#FF9800')
         # self.p_dt_end.pack(fill=tk.NONE, expand=1, anchor="n", side=tk.TOP)
         self.p_dt.add(self.p_dt_end)
         # Showing end date.
-        end_date_indicator = tk.Label(self.p_dt_end, text="End Date:")
-        end_date_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
-        end_date_label = tk.Label(self.p_dt_end, textvariable=self.end_date)
-        end_date_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        end_date_indicator = tk.Label(self.p_dt_end, text="End Date:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        end_date_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
+        end_date_label = tk.Label(self.p_dt_end, textvariable=self.end_date, bg="#FF9800", fg="#FFF9C4", font=('Times', 13, 'bold'))
+        end_date_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Showing start time.
-        end_time_indicator = tk.Label(self.p_dt_end, text="End time:")
-        end_time_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
-        end_time_label = tk.Label(self.p_dt_end, textvariable=self.end_time)
-        end_time_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        end_time_indicator = tk.Label(self.p_dt_end, text="End time:", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
+        end_time_indicator.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
+        end_time_label = tk.Label(self.p_dt_end, textvariable=self.end_time, bg="#FF9800", fg="#FFF9C4", font=('Times', 13, 'bold'))
+        end_time_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
         # Choose start date and time.
-        end_date_btn = tk.Button(self.p_dt_end, text="Choose end date & time", command=self.choose_end_date_and_time)
-        end_date_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="n")
+        end_date_btn = tk.Button(self.p_dt_end, text="Choose end date & time", command=self.choose_end_date_and_time, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
+        end_date_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20)
 
         # Create a PanedWindow for plotting.
-        self.p_plot = tk.PanedWindow(self)
+        self.p_plot = tk.PanedWindow(self, bg='#FF9800')
         self.p_control.add(self.p_plot)
         # Choose plotting df names.
         self.plot_df = tk.StringVar()
         self.plot_df.set("All")
-        choose_plot_name_label = tk.Label(self.p_plot, text="The name of csv for Plotting")
+        choose_plot_name_label = tk.Label(self.p_plot, text="The name of csv for Plotting", bg="#FF9800", fg="#757575", font=('Times', 15, 'bold'))
         choose_plot_name_label.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="center")
-        choose_plot_name_entry = tk.Entry(self.p_plot, textvariable=self.plot_df, state="readonly")
+        choose_plot_name_entry = tk.Entry(self.p_plot, textvariable=self.plot_df, state="readonly", bg="#FF9800")
         choose_plot_name_entry.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="center")
-        choose_plot_name_btn = tk.Button(self.p_plot, text="Choose plotting dataframe", command=self.choose_plotting_df_name)
+        choose_plot_name_btn = tk.Button(self.p_plot, text="Choose plotting dataframe", command=self.choose_plotting_df_name, bg="#FF9800", fg="#FFF9C4", font=('Times', 10, 'bold'))
         choose_plot_name_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="center")
         # Create plotting for common.
-        plot_average_btn = tk.Button(self.p_plot, text="Plot Day Average Temperature vs Date", command=self.plotting_average, bg="green", fg="white")
-        plot_average_btn.pack(fill=tk.NONE, side=tk.LEFT, padx=20, pady=20, anchor="center")
-        plot_each_day_btn = tk.Button(self.p_plot, text="Plot Temperature vs Time in different date", command=self.plotting_each_day, bg="green", fg="white")
+        plot_each_day_btn = tk.Button(self.p_plot, text="Plot Temperature vs Time in different date", command=self.plotting_each_day, bg="#FF9800", fg="#212121", font=('Times', 10, 'bold'))
         plot_each_day_btn.pack(fill=tk.NONE, side=tk.RIGHT, padx=20, pady=20, anchor="center")
+        plot_average_btn = tk.Button(self.p_plot, text="Plot Day Average Temperature vs Date", command=self.plotting_average, bg="#FF9800", fg="#212121", font=('Times', 10, 'bold'))
+        plot_average_btn.pack(fill=tk.NONE, side=tk.RIGHT, padx=20, pady=20, anchor="center")
         # Create plotting with recommendations.
-        self.p_reco = tk.PanedWindow(self)
+        self.p_reco = tk.PanedWindow(self, bg='#FF9800')
         self.p_control.add(self.p_reco)
         # Botton for plotting with recommended range.
-        reco_each_day_btn = tk.Button(self.p_reco, text="Plot Temperature vs Time in different date with recommended range", command=self.plotting_each_day_with_reco, bg="green", fg="white")
+        reco_each_day_btn = tk.Button(self.p_reco, text="Plot Temperature vs Time in different date with recommended range", command=self.plotting_each_day_with_reco, bg="#FF9800", fg="#212121", font=('Times', 10, 'bold'))
         reco_each_day_btn.pack(fill=tk.NONE, side=tk.RIGHT, padx=20, pady=20, anchor="center")
-        reco_average_btn = tk.Button(self.p_reco, text="Plot Day Average Temperature vs Date with recommended range", command=self.plotting_average_with_reco, bg="green", fg="white")
+        reco_average_btn = tk.Button(self.p_reco, text="Plot Day Average Temperature vs Date with recommended range", command=self.plotting_average_with_reco, bg="#FF9800", fg="#212121", font=('Times', 10, 'bold'))
         reco_average_btn.pack(fill=tk.NONE, side=tk.RIGHT, padx=20, pady=20, anchor="center")
 
 
@@ -208,14 +210,14 @@ class DataPlotting(tk.Tk):
         # Quit button.
         # ------------
         goodbye_button = tk.Button(self, text="Quit",
-                                    command=self.say_goodbye, bg="green", fg="white")
+                                    command=self.say_goodbye, bg="#FF9800", fg="#FFE0B2", font=('Times', 10, 'bold'))
         goodbye_button.pack(side=tk.BOTTOM, padx=(0, 20), pady=(0, 20))
 
         #=============
         # Testing
         #=============
-        show_end_date_btn = tk.Button(self, text="Testing", command=self.show_self_end_date)
-        show_end_date_btn.pack(side=tk.BOTTOM)
+        # show_end_date_btn = tk.Button(self, text="Testing", command=self.show_self_end_date, bg="#FF9800")
+        # show_end_date_btn.pack(side=tk.BOTTOM)
 
     def select_directory_input(self):
         """
@@ -511,11 +513,11 @@ class DataPlotting(tk.Tk):
         self.end_date = date
         self.end_time = time
 
-    def show_self_end_date(self):
-        """
-        For testing
-        """
-        print(self.plot_df.get())
+    # def show_self_end_date(self):
+    #     """
+    #     For testing
+    #     """
+    #     print(self.plot_df.get())
 
      
 
